d919d634cc8012851d991e910c100966567be86d
kimwoohyun <dngus242@gmail.com>
2018-01-03 17:51:54 +0900

    Init structure

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="demo">
+    sfdgf
+
+    sss
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style scoped>
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+	entry: {
+    entry: './index.js'
+  },
+	output: {
+    filename: 'dist/bundle.js'
+  },
+	module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ]
+  }
+};
